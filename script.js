6a2dd576743a2c904071017e7d1df08a9fe763f0
Logan Woolf <loganwoolf@gmail.com>
2021-07-05 00:21:27 -0600

    create, add toggle fn to menu button listener

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menu = document.querySelector('.hamburger');
+const menuFlyout = document.querySelector('.menu-flyout');
+
 
 function toggleMenu() {
    console.log('click');
-
+   if (menuFlyout.classList.contains('hidden')) {
+      menuFlyout.classList.remove('hidden');
+   } else {
+      menuFlyout.classList.add('hidden');
+   }
 }
 
 
